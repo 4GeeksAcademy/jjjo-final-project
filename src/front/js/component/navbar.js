d3e3da6805a0a19b0ea4d47ff45876063a2aac00
omararcx <100163833+omararcx@users.noreply.github.com>
2023-11-25 01:41:47 +0000

    changes on nav bar, trying to update an image

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,7 +7,9 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 			<div className="container-fluid">
-				<a className="navbar-brand" href="#">Logo</a>
+				<Link to="/home">
+					<img className="my-img p-2" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Ffree-png-vectors%2Fman-pictogram&psig=AOvVaw2jmh94r2WoYwLwBQyPspnl&ust=1700948680537000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCR5-HN3YIDFQAAAAAdAAAAABAJ" alt="A person studying"></img>
+				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
