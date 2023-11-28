be67509bc8e0ec75821e0e0c928e3a56e2a72a97
josefrometa <73325073+josefrometa@users.noreply.github.com>
2023-11-28 02:52:47 +0000

    agregado home, footer y creando vistas

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <p>&copy; 2023 Mi Compañía. Todos los derechos reservados.</p>
+      </div>
+      <div class="col-md-6">
+        <p class="text-right">Contáctanos: <a href="mailto:maildelaApp@gmail.com">maildelaApp@gmail.com</a></p>
+        <p className="">Numero de contacto: +584149286826</p>
+      </div>
+    </div>
+  </div>
 	</footer>
 );
+export default Footer
---
+++ src/front/js/pages/readmore.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Readmore = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="container d-flex flex-column">
+      <div className="row">
+        <div className="col-6">
+          <h1 className="display-2">Que es (Nombre de la app)?</h1>
+        </div>
+        <div className="row">
+          <div className="col-12 border border-primary rounder">(Nombre de la aplicacion) es una aplicacion con el fin de hacer mucho mas sencillo encontrar
+          profesores u alumnos, </div>
+        </div>
+      </div>
+    </div>
+   
+    );        
+};
