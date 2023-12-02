fb7e8e3f63c2f2e4facfe11ab9433e29edb0ecca
josefrometa <73325073+josefrometa@users.noreply.github.com>
2023-12-02 01:46:09 +0000

    cambiadas las etiquetas mal puetas

--- src/api/routes.py
+++ src/api/routes.py
@@ -97,45 +97,52 @@ def login(): #Capaz poner un nombre mas intuitivo
             else:
                 return jsonify({"Message":"Datos incorrectos"}), 400
             
+
+# Un metodo get para comprobar que los favoritos del usurio se estan creando
 @api.route ('user/favorites/int:theid', methods=['GET'])
 def get_user_favorites(theid=None):
     if theid is None:
         return jsonify({"Message":"This user does not exist"}), 404
     
     favorites = Favorites.query.get.filter_by(user_id=theid).all()
+
     if favorites is None:
         return jsonify({"Message":"This user has no favorites"})
+    
     favorites_list = []
+
     for item in favorites:
         favorites_list.append(item.serialize())
     return jsonify(favorites_list), 200
 
-@api.route ('favorites/user/<int:user_added>/<int:user_id>', methods=['POST'])
-def add_favorites_users(user_added, user_id):
-    favorites = Favorites.query.filter_by(user_id = user_added, user_id = user_id).first()
 
-    user = User.query.get(user_id)
+# End point para crear favoritos
+@api.route ('favorites/user/<int:instructor_id>/<int:student_id>', methods=['POST'])
+def add_favorites_users(instructor_id, student_id):
+    favorites = Favorites.query.filter_by(instructor_id = instructor_id, student_id = student_id).first()
+
+    user = User.query.get(instructor_id)
     if user is None:
         return jsonify({"Message":"This user does not exist"}), 404
     
     if favorites is not None:
-        return jsonify({"Message":"This favorite alredy exist"}), 
-# Buscar que error va aqui
+        return jsonify({"Message":"This favorite alredy exist"}), 400
 
-    add_favorite = Favorites(user_id = user_id, user_added = user_id)
+    add_favorite = Favorites(student_id = student_id, instructor_id = instructor_id)
     db.sessions.add(add_favorite)
 
     try:
         db.session.commit()
         return  jsonify({"Message":"The favorite was added"}), 200
-# aqui seria 200 o 201?
+
     except Exception as error:
         db.session.rollback()
         return jsonify({"Message":f"{error}"}), 500
 
-api.route ('favorites/user/<int:user_added>/<int:user_id>', methods=['DELETE'])
-def delete_favorite(user_added, user_id):
-    favorite = Favorites.query.filter_by(user_id=user_id, user_id=user_added).first()
+# End point para borrar favoritos creados
+api.route ('favorites/user/<int:instructor_id>/<int:student_id>', methods=['DELETE'])
+def delete_favorite(instructor_id, student_id):
+    favorite = Favorites.query.filter_by(student_id=student_id, user_id=instructor_id).first()
 
     if favorite is None:
         return jsonify({"Message":"This favorite does not exist"}), 404
@@ -144,6 +151,7 @@ def delete_favorite(user_added, user_id):
         db.session.delete(favorite)
         db.session.commit()
         return jsonify({"Message":"This favorite was deleted"})
+    
     except Exception as error:
         db.session.rollback()
         return jsonify({"Message":f"{error}"}), 500
